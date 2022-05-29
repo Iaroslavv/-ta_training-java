fb8d85891a3d4a3d5d0f0d77dbfbdbb1c369cd9e
Iaroslav <yaroslaw.bulimov@yandex.ru>
2022-05-29 23:51:32 +0200

    module_1

---
+++ src/main/java/module_1/HelloAutocode.java
@@ -0,0 +1,7 @@
+package module_1;
+
+public class HelloAutocode {
+    public static void main( String[] args ) {
+        System.out.println( "Hello, Autocode!" );
+    }
+}
---
+++ src/main/java/module_1/MeetAStranger.java
@@ -0,0 +1,12 @@
+package module_1;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main (String args[] ) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        System.out.println("Hello, " + name);
+    }
+}
