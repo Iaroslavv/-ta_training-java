b6e396c0c79357cbab901f37ae3756ae41a2f6f2
Iaroslav <yaroslaw.bulimov@yandex.ru>
2022-05-31 23:47:45 +0200

    fix pizzasplit module_3

--- src/main/java/com/epam/training/iaroslav_bulimov/module_3/PizzaSplit.java
+++ src/main/java/com/epam/training/iaroslav_bulimov/module_3/PizzaSplit.java
@@ -1,12 +1,18 @@
 package com.epam.training.iaroslav_bulimov.module_3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PizzaSplit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numPeople = sc.nextInt();
-        int pizzaSlices = sc.nextInt();
-        //  TO DO:
+        int people = sc.nextInt();
+        int pizzas = sc.nextInt();
+        int pizzaCounter = 1;
+        while ((pizzas*pizzaCounter) % people != 0) {
+            ++pizzaCounter;
+        }
+        System.out.println(pizzaCounter);
     }
 }
+
