40995096a9edd162ae72d4980f95e50447b75f61
Iaroslav <yaroslaw.bulimov@yandex.ru>
2022-05-31 19:21:20 +0200

    module_3 todo pizzasplit

---
+++ src/main/java/com/epam/training/iaroslav_bulimov/module_3/PizzaSplit.java
@@ -0,0 +1,12 @@
+package com.epam.training.iaroslav_bulimov.module_3;
+
+import java.util.Scanner;
+
+public class PizzaSplit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numPeople = sc.nextInt();
+        int pizzaSlices = sc.nextInt();
+        //  TO DO:
+    }
+}
