ccafab7937665c237ad52c91dac365e887bf4044
Iaroslav <yaroslaw.bulimov@yandex.ru>
2022-06-07 23:07:28 +0200

    module_5 line intersection

---
+++ src/main/java/com/epam/training/iaroslav_bulimov/module_5/line_intersection/Line.java
@@ -0,0 +1,28 @@
+package com.epam.training.iaroslav_bulimov.module_5.line_intersection;
+
+public class Line {
+
+    int k;
+    int b;
+    //constructors
+    public Line() {}
+
+    public Line(int k, int b) {
+        this.k = k;
+        this.b = b;
+    }
+
+    //method, type of return object is Point
+    public Point intersection(Line other) {
+        //throw new UnsupportedOperationException();
+        int x, y;
+        if (k == other.k) {
+            return null;
+        }
+        x = (other.b - b) / (k - other.k);
+        y = k * x + b;
+        return new Point(x, y);
+    }
+
+}
+
