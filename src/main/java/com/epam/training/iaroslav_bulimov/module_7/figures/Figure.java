50b06bc01f37402667cd173505cae83a24655e97
Iaroslav <yaroslaw.bulimov@yandex.ru>
2022-06-07 23:17:58 +0200

    module_7 figures

---
+++ src/main/java/com/epam/training/iaroslav_bulimov/module_7/figures/Figure.java
@@ -0,0 +1,16 @@
+package com.epam.training.iaroslav_bulimov.module_7.figures;
+
+
+abstract class Figure{
+
+    public abstract double area();
+
+    public abstract String pointsToString();
+
+    public String toString() {
+        return this.getClass().getSimpleName() + "[" + pointsToString() + "]";
+    }
+
+    public abstract Point leftmostPoint();
+}
+
