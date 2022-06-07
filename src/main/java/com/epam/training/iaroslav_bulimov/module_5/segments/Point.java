1fe15b2f94944e4387ee0c290e58d16505cdd04e
Iaroslav <yaroslaw.bulimov@yandex.ru>
2022-06-07 23:09:13 +0200

    module_5 segments

---
+++ src/main/java/com/epam/training/iaroslav_bulimov/module_5/segments/Point.java
@@ -0,0 +1,21 @@
+package com.epam.training.iaroslav_bulimov.module_5.segments;
+
+
+class Point {
+    private double x;
+    private double y;
+
+    public Point(final double x, final double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
+
