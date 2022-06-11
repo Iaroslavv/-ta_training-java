30968f818a5d7c0310e7d4bfb026e03ed8393e92
Iaroslav <yaroslaw.bulimov@yandex.ru>
2022-06-11 19:11:17 +0200

    module_4 multiplymatrix

---
+++ src/main/java/com/epam/training/iaroslav_bulimov/module_4/MultiplyMatrix.java
@@ -0,0 +1,39 @@
+package com.epam.training.iaroslav_bulimov.module_4;
+
+import java.util.Arrays;
+
+public class MultiplyMatrix {
+    public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+
+        int[][] result = new int[matrix1.length][matrix2[0].length];
+
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix2[0].length; j++) {
+                for (int r = 0; r < matrix1[0].length; r++) {
+                    result[i][j] += matrix1[i][r] * matrix2[r][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Test your code here!\n");
+
+        // Get a result of your code
+
+        int[][] a = {
+                {0, 12345},
+                {4509, 0},
+                {3, 567} };
+
+        int[][] b = {
+                {653, 0, 25353},
+                {0, 61, 6} };
+
+        int[][] result = multiply(a, b);
+        System.out.println(Arrays.deepToString(result).replace("],", "]\n"));
+    }
+}
